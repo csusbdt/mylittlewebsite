dd9269369a10f037170df873fa49f4298ad17f67
David Turner <csusbdt@gmail.com>
2023-05-26 11:45:40 -1000

    X

--- docs/lines/lines.js
+++ docs/lines/lines.js
@@ -1,3 +1,4 @@
+
 const dash = [
 	i_dash_0 , i_dash_1 , i_dash_2 , i_dash_3 , i_dash_4 , i_dash_5 , i_dash_6 , i_dash_7 , i_dash_8,
 	i_dash_9 , i_dash_10, i_dash_11, i_dash_12, i_dash_13, i_dash_14, i_dash_15, i_dash_16
@@ -9,6 +10,20 @@ let green_i           = 0    ;
 let black_dot_i       = 0    ;
 let red_dot_i         = null ;
 let dash_i            = null ;
+
+let audio = null ;
+let gain = null;
+let gs   = [];
+
+const init_audio = _ => {
+	// this function must run in click handler to work on apple hardware
+	if (audio === null) audio = new (window.AudioContext || window.webkitAudioContext)();
+	if (audio.state === "suspended") audio.resume();
+	if (gain !== null) return;
+	gain = audio.createGain();
+	gain.gain.value = .5;
+	gain.connect(audio.destination);
+};
 
 const start = _ => {
 	set_design_size(1000, 1000);
@@ -25,6 +40,20 @@ const stop = _ => {
 const click = e => {
 	const p = design_coords(e);
 	if (is_inside_circle(828, 156, 43, p)) {
+		init_audio();
+		let f = 60;
+		for (let i = 0; i < dash.length; ++i) {
+			let o = audio.createOscillator();
+			let g = audio.createGain();
+			o.connect(g);
+			o.frequency.value = f;
+			o.start();
+			g.gain.value = 0;
+			g.gain.setTargetAtTime(1, audio.currentTime + i * .35, .1);
+			g.connect(gain);
+			f += 8;
+			gs.push(g);
+		}
 		dash_i = 0;
 	}
 };
@@ -36,6 +65,9 @@ const loop = _ => {
 			++dash_i;
 		} else {
 			red_dot_i = 0;
+			for (let i = dash.length - 1; i >= 0; --i) {
+				gs[i].gain.setTargetAtTime(0, audio.currentTime + i * .35, .1);
+			}
 		}
 	}
 };
