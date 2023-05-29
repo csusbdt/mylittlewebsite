869d414654d0e276ef67b4f669e41c9e5eb7066d
David Turner <csusbdt@gmail.com>
2023-05-28 19:56:14 -1000

    X

--- docs/test/index.js
+++ docs/test/index.js
@@ -2,18 +2,29 @@ import "../main.js";
 import { init as init_audio } from "../music/audio.js";
 
 let loop_id = null;
-const color = [i_blue, i_red, i_yellow, i_green, i_white, i_black];
-let color_i = 0;
+
+const audio_color = [i_audio_yellow, i_audio_white];
+let audio_i = 0;
 
 const click = e => {
     const p = design_coords(e);
-    if (++color_i === 6) color_i = 0;
+    if (++audio_i === 2) {
+        audio_i = 0;
+        draw(audio_color[audio_i]);
+        draw(i_audio_border);
+    }
 };
 
 canvas.addEventListener('click', click);
 
 const loop = _ => {
-    draw(color[color_i]);	
+    draw(i_blue);
+    draw(i_menu_red);
+    draw(i_menu_border);
+    draw(i_back_yellow);
+    draw(i_back_border);
+    draw(audio_color[audio_i]);
+    draw(i_audio_border);
 };
 
 loop();
