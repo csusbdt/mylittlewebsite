ecfeec2fb7c823361bfd37ca1df632ef9db67a66
David Turner <csusbdt@gmail.com>
2023-06-02 18:22:49 -1000

    X

--- docs/space_shooter/index.js
+++ docs/space_shooter/index.js
@@ -1,57 +1,62 @@
 import start_home  from "../home/index.js" ;
 
-const i_portal_0 = image("/images/portal_0.png");
-const i_portal_1 = image("/images/portal_1.png");
-const i_portal_2 = image("/images/portal_2.png");
-const i_portal_3 = image("/images/portal_3.png");
-const i_blue_dot = image("/images/blue_dot.png");
+const i_portal_0          = image("/images/portal_0.png"          );
+const i_portal_1          = image("/images/portal_1.png"          );
+const i_portal_2          = image("/images/portal_2.png"          );
+const i_portal_3          = image("/images/portal_3.png"          );
+const i_blue_dot          = image("/images/blue_dot.png"          );
 
-const i_ship_left = image("/images/ship_left.png"     );
-const i_ship_middle = image("/images/ship_middle.png" );
-const i_ship_right = image("/images/ship_right.png"   );
+const i_ship_left         = image("/images/ship_left.png"         );
+const i_ship_middle       = image("/images/ship_middle.png"       );
+const i_ship_right        = image("/images/ship_right.png"        );
 
-const i_bullet_left_0 = image("/images/bullet_left_0.png");
-const i_bullet_left_1 = image("/images/bullet_left_1.png");
+const i_bullet_left_0     = image("/images/bullet_left_0.png"     );
+const i_bullet_left_1     = image("/images/bullet_left_1.png"     );
 
-const i_bullet_right_0 = image("/images/bullet_right_0.png"  );
-const i_bullet_right_1 = image("/images/bullet_right_1.png"      );
+const i_bullet_right_0    = image("/images/bullet_right_0.png"    );
+const i_bullet_right_1    = image("/images/bullet_right_1.png"    );
 
-const i_gun_left_border = image("/images/gun_left_border.png"    );
-const i_gun_left_blue = image("/images/gun_left_blue.png"        );
-const i_gun_left_red = image("/images/gun_left_red.png"          );
+const i_gun_left_border   = image("/images/gun_left_border.png"   );
+const i_gun_left_blue     = image("/images/gun_left_blue.png"     );
+const i_gun_left_red      = image("/images/gun_left_red.png"      );
 
-const i_gun_right_border = image("/images/gun_right_border.png"  );
-const i_gun_right_blue = image("/images/gun_right_blue.png"      );
-const i_gun_right_red = image("/images/gun_right_red.png"        );
+const i_gun_right_border  = image("/images/gun_right_border.png"  );
+const i_gun_right_blue    = image("/images/gun_right_blue.png"    );
+const i_gun_right_red     = image("/images/gun_right_red.png"     );
 
-const i_explosion_left_0 = image("/images/explosion_left_0.png"  );
-const i_explosion_left_1 = image("/images/explosion_left_1.png"  );
-const i_explosion_left_2 = image("/images/explosion_left_2.png"  );
+const i_explosion_left_0  = image("/images/explosion_left_0.png"  );
+const i_explosion_left_1  = image("/images/explosion_left_1.png"  );
+const i_explosion_left_2  = image("/images/explosion_left_2.png"  );
 
 const i_explosion_right_0 = image("/images/explosion_right_0.png" );
 const i_explosion_right_1 = image("/images/explosion_right_1.png" );
 const i_explosion_right_2 = image("/images/explosion_right_2.png" );
 
-const i_back = image("/images/back.png");
+const i_back              = image("/images/back.png"              );
 
-let update_id = null;
+const inside_portal       = circle(183, 212, 45)     ;
+const inside_exit         = circle(326,  55, 32)     ;
+const inside_gun_left     = rect( 22, 228, 126, 372) ;
+const inside_gun_right    = rect(235, 215, 344, 360) ;
 
-let ship_i            = 0      ; // 0, 1, 2, 3, null
-let explosion_left_i  = null   ; // 0, 1, 2, null
-let explosion_right_i = null   ; // 0, 1, 2, null
-let bullet_left_i     = null   ; // 0, 1, 2, null
-let bullet_right_i    = null   ; // 0, 1, 2, null
-let gun_left_i        = 0      ; // 0, 1, null
-let gun_right_i       = 0      ; // 0, 1, null
-let portal_i          = 0      ; // 0, 1, 2, 3, null, 4, 5, 6
-let blue_dot_i        = 0      ; // 0, null
+let update_id             = null   ;
+
+let ship_i                = 0      ; 
+let explosion_left_i      = null   ; 
+let explosion_right_i     = null   ; 
+let bullet_left_i         = null   ; 
+let bullet_right_i        = null   ; 
+let gun_left_i            = 0      ; 
+let gun_right_i           = 0      ; 
+let portal_i              = 0      ; 
+let blue_dot_i            = 0      ; 
 
 const click = e => {
 	const p = design_coords(e);
 	if (portal_i === 0) {
-		if (circle(183, 212, 45)(p)) {
+		if (inside_portal(p)) {
 			portal_i = 1;
-    	} else if (circle(326, 55, 32)(p)) {
+    	} else if (inside_exit(p)) {
 			stop();
             start_home();
     	}
@@ -62,10 +67,10 @@ const click = e => {
         bullet_left_i     === null && 
         bullet_right_i    === null  
     ) {
-        if (rect(22, 228, 126, 372)(p)) {
+        if (inside_gun_left(p)) {
             bullet_left_i = 0;
             gun_left_i    = 1;
-        } else if (rect(235, 215, 344, 360)(p)) {
+        } else if (inside_gun_right(p)) {
             bullet_right_i = 0;
             gun_right_i    = 1;
         }
@@ -85,16 +90,89 @@ const start = _ => {
 	blue_dot_i        = 0      ;
 	canvas.addEventListener('click', click);
 	update_id = setInterval(update, 350);
+	update();
 };
 
 const stop = _ => {
 	canvas.removeEventListener('click', click);
 	clearInterval(update_id);
-	update_id  = null   ;
+	update_id = null;
 };
 
 const update = _ => {
-	draw();
+    bg_green();
+
+	if (ship_i === 0) {
+		draw(i_ship_left);
+	} else if (ship_i === 1 || ship_i === 3) {
+		draw(i_ship_middle);
+	} else if (ship_i === 2) {
+		draw(i_ship_right);
+	}
+
+	if (explosion_left_i === 0) {
+		draw(i_explosion_left_0);
+	} else if (explosion_left_i === 1) {
+		draw(i_explosion_left_1);
+	} else if (explosion_left_i === 2) {
+		draw(i_explosion_left_2);
+	}
+	
+	if (explosion_right_i === 0) {
+		draw(i_explosion_right_0);
+	} else if (explosion_right_i === 1) {
+		draw(i_explosion_right_1);
+	} else if (explosion_right_i === 2) {
+		draw(i_explosion_right_2);
+	}
+
+	draw(i_gun_left_border);
+	if (gun_left_i === 0) {
+		draw(i_gun_left_blue);
+	} else {
+		draw(i_gun_left_red);
+	}
+
+	draw(i_gun_right_border);
+	if (gun_right_i === 0) {
+		draw(i_gun_right_blue);
+	} else {
+		draw(i_gun_right_red);
+	}
+
+	if (bullet_left_i === 0) {
+		draw(i_bullet_left_0);		
+	} else if (bullet_left_i === 1) {
+		draw(i_bullet_left_1);		
+	} else if (bullet_left_i === 2) {
+		draw(i_bullet_left_0, 0, -140);		
+	} else if (bullet_right_i === 0) {
+		draw(i_bullet_right_0);		
+	} else if (bullet_right_i === 1) {
+		draw(i_bullet_right_1);		
+	} else if (bullet_right_i === 2) {
+		draw(i_bullet_right_0, 0, -120);
+	} 
+	
+	if (portal_i === 0) {
+		draw(i_portal_0);
+	} else if (portal_i === 1) {
+		draw(i_portal_1);
+	} else if (portal_i === 2) {
+		draw(i_portal_2);
+	} else if (portal_i === 3) {
+		draw(i_portal_3);
+	} else if (portal_i === 4) {
+		draw(i_portal_3);
+	} else if (portal_i === 5) {
+		draw(i_portal_2);
+	} else if (portal_i === 6) {
+		draw(i_portal_1);
+	}
+
+	if (blue_dot_i === 0) {
+		draw(i_blue_dot);
+	} 
 
 	if (ship_i !== null && ++ship_i === 4) ship_i = 0;
 
@@ -160,82 +238,6 @@ const update = _ => {
 		ship_i = Math.floor(Math.random() * 4);
 		return;
 	}
-};
-
-const draw = _ => {
-    bg_green();
-
-	if (ship_i === 0) {
-		ctx.drawImage(i_ship_left, 0, 0);
-	} else if (ship_i === 1 || ship_i === 3) {
-		ctx.drawImage(i_ship_middle, 0, 0);
-	} else if (ship_i === 2) {
-		ctx.drawImage(i_ship_right, 0, 0);
-	}
-
-	if (explosion_left_i === 0) {
-		ctx.drawImage(i_explosion_left_0, 0, 0);
-	} else 	if (explosion_left_i === 1) {
-		ctx.drawImage(i_explosion_left_1, 0, 0);
-	} else 	if (explosion_left_i === 2) {
-		ctx.drawImage(i_explosion_left_2, 0, 0);
-	}
-	
-	if (explosion_right_i === 0) {
-		ctx.drawImage(i_explosion_right_0, 0, 0);
-	} else 	if (explosion_right_i === 1) {
-		ctx.drawImage(i_explosion_right_1, 0, 0);
-	} else 	if (explosion_right_i === 2) {
-		ctx.drawImage(i_explosion_right_2, 0, 0);
-	}
-
-	ctx.drawImage(i_gun_left_border, 0, 0);
-	if (gun_left_i === 0) {
-		ctx.drawImage(i_gun_left_blue, 0, 0);
-	} else {
-		ctx.drawImage(i_gun_left_red, 0, 0);
-	}
-
-	ctx.drawImage(i_gun_right_border, 0, 0);
-	if (gun_right_i === 0) {
-		ctx.drawImage(i_gun_right_blue, 0, 0);
-	} else {
-		ctx.drawImage(i_gun_right_red, 0, 0);
-	}
-
-	if (bullet_left_i === 0) {
-		ctx.drawImage(i_bullet_left_0, 0, 0);		
-	} else if (bullet_left_i === 1) {
-		ctx.drawImage(i_bullet_left_1, 0, 0);		
-	} else if (bullet_left_i === 2) {
-		ctx.drawImage(i_bullet_left_0, 0, -140);		
-	} else if (bullet_right_i === 0) {
-		ctx.drawImage(i_bullet_right_0, 0, 0);		
-	} else if (bullet_right_i === 1) {
-		ctx.drawImage(i_bullet_right_1, 0, 0);		
-	} else if (bullet_right_i === 2) {
-		ctx.drawImage(i_bullet_right_0, 0, -120);
-	} 
-	
-	if (portal_i === 0) {
-		ctx.drawImage(i_portal_0, 0, 0);
-	} else if (portal_i === 1) {
-		ctx.drawImage(i_portal_1, 0, 0);
-	} else if (portal_i === 2) {
-		ctx.drawImage(i_portal_2, 0, 0);
-	} else if (portal_i === 3) {
-		ctx.drawImage(i_portal_3, 0, 0);
-	} else if (portal_i === 4) {
-		ctx.drawImage(i_portal_3, 0, 0);
-	} else if (portal_i === 5) {
-		ctx.drawImage(i_portal_2, 0, 0);
-	} else if (portal_i === 6) {
-		ctx.drawImage(i_portal_1, 0, 0);
-	}
-
-	if (blue_dot_i === 0) {
-		ctx.drawImage(i_blue_dot, 0, 0);
-	} 
 };
 
 export default start;
